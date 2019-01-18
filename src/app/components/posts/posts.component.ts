33ebdad33971d1739917f6f99e7f91d805d1a82f
astonished12 <dan_cehan@yahoo.com>
2019-01-18 10:48:49 +0200

    @Input @Output Event Emitter example with change info between components app-form-post app-posts

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -11,6 +11,13 @@ import { PostService } from '../../services/post.service';
 export class PostsComponent implements OnInit {
 
   posts: Post[];
+  currentPost: Post = {
+    id:0,
+    title: '',
+    body: ''
+  }
+
+  isEdit:boolean = false;
 
   constructor(private postService: PostService) { }
 
@@ -22,6 +29,11 @@ export class PostsComponent implements OnInit {
 
   onNewPost(post: Post) {
     this.posts.unshift(post);
+  }
+
+  editPost(post: Post){
+    this.currentPost = post;
+    this.isEdit = true;
   }
 
 }
